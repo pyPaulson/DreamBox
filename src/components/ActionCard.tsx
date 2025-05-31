bc00ddecf98db70cff773a0a03bf068f26ddac4e
pyPaulson <agyekumpaul07@gmail.com>
2025-05-31 17:55:40 +0000

    changes

--- src/components/ActionCard.tsx
+++ src/components/ActionCard.tsx
@@ -1,5 +1,5 @@
 import { View, Text, StyleSheet } from "react-native";
-import { Ionicons } from "@expo/vector-icons";
+// import { Ionicons } from "@expo/vector-icons";
 import AppColors from "@/constants/AppColors";
 import Fonts from "@/constants/Fonts";
 
